6177049e719edb07b4039055b71359173945eabd
BanhsBao <45170709+banhsbao@users.noreply.github.com>
2021-09-08 14:07:57 +0700

    Update Helm_Uninstall_All_Service.sh

--- deployment/Helm_Uninstall_All_Service.sh
+++ deployment/Helm_Uninstall_All_Service.sh
@@ -4,6 +4,6 @@ helm uninstall authentication-service
 helm uninstall employee-service
 helm uninstall admin-service
 helm uninstall manager-service
-helm uninstall cdo-webclient
+helm uninstall client-service
 echo "Press any key to close"
 read junk
